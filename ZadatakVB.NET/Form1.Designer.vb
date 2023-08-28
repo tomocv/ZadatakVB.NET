0aee6f21e3d1838ef36f648afa43c0dde61b95c2
Tomo <Tomo@DESKTOP-C079G1P>
2023-08-28 14:03:29 +0200

    Add project files.

---
+++ ZadatakVB.NET/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(70, 70)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(34, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Ime"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(70, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(62, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Prezime"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(70, 160)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(52, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "E-mail"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(144, 64)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(156, 27)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(144, 108)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(156, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(144, 157)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(156, 27)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(144, 216)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 6
+        Button1.Text = "&Spremi"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "VB.NET Zadatak"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
